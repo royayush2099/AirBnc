918e44eaa76673748ee5ceade18982c16bb3542e
Ayushroy <royayush2099@gmail.com>
2024-10-01 19:12:38 +0530

    Register and Login Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import  { Routes,Route } from "react-router-dom"
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
 import Layout from './Layout'
+import RegisterPage from './pages/RegisterPage'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Route path='/' element={<Layout/>}>
       <Route index element={<IndexPage/>}/>
 <Route path='/login' element={<LoginPage/>}/>
+<Route path='/register' element={<RegisterPage/>}/>
       </Route>
 
     </Routes>
--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div>
+    <div className='p-4 flex flex-col min-h-screen'>
       <Header/>
       <Outlet/>
     </div>
